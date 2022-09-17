6de554153e93a7cf307210436298df10c54f2895
AN ❦ <108299225+krakxn@users.noreply.github.com>
2022-09-17 11:30:22 +0530

    various errors
    accounts for the spelling mistakes in html file; also spelling mistakes specifically in this file (e.g. `listenForTransationMined`)

--- script.js
+++ script.js
@@ -1,4 +1,94 @@
-import { ethers } from "./ethers.js";
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const withdrawButton = document.getElementById("withdrawButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+connectButton.onclick = connect
+withdrawButton.onclick = withdraw
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") { // Checks if client has Metamask
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" }) // Requests for account signing -a Metamask pop-up (under the hood: An array of a single, hexadecimal Ethereum address string.)
+    } catch (error) {
+      console.log(error)
+    }
+    connectButton.innerHTML = "Connected" // Changes the button's content to Connected if an error is not thrown
+    const accounts = await ethereum.request({ method: "eth_accounts" }) //  Returns a list of addresses owned by client.
+    console.log(accounts)
+  } else {
+    connectButton.innerHTML = "Please install MetaMask" // If no Metamask is detected
+  }
+}
+
+async function withdraw() {
+  console.log(`Withdrawing...`)
+  if (typeof window.ethereum !== "undefined") { // Checks if client has Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum) // Similar to a RPC provider, acts like a provider
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+      const transactionResponse = await contract.withdraw()
+      await listenForTransactionMine(transactionResponse, provider) // Returns a promise (either resolved or rejected)
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    withdrawButton.innerHTML = "Please install MetaMask" // If client does not have metamask
+  }
+}
+
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value
+  console.log(`Funding with ${ethAmount}...`)
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      await listenForTransactionMine(transactionResponse, provider)
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    try {
+      const balance = await provider.getBalance(contractAddress)
+      console.log(ethers.utils.formatEther(balance))
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    balanceButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`)
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(
+        `Completed with ${transactionReceipt.confirmations} confirmations. `
+      )
+      resolve()
+    })
+  })
+}
+
+/*import { ethers } from "./ethers.js";
 import { abi, contractAddress } from "./constants.js";
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
@@ -82,7 +172,7 @@ async function widthdraw() {
       console.log(error);
     }
   }
-}
+} */
 
 // async function withdraw() {
 //   console.log(`Withdrawing...`);
